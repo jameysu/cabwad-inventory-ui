c31c0293dfb698732818dabee0821668ecd60ed7
jems <jamjamcansado@gmail.com>
2025-10-01 11:28:09 +0800

    Add logout button

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,20 @@
+import { Button } from "antd";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("auth");
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <Button onClick={logout}>Log out</Button>
+    </div>
+  );
 };
 
 export default Dashboard;
